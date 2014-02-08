028b593a376f82ea1ec9bf53f96e7e0dbc4cef71
Martin Eve <martin@martineve.com>
2014-02-08 14:40:04 +0000

    Fix for issue handling in journal
    PEP8 indentation (spaces)

--- eprintsCV.py
+++ eprintsCV.py
@@ -20,107 +20,142 @@ import urllib2
 import json
 from datetime import datetime
 
+
 def printStart():
-	print "<ul>"
+    print "<ul>"
+
 
 def printEnd():
-	print "</ul>"
+    print "</ul>"
+
 
 def printableHeading(x):
-	return {
-		'book': "Books",
-		'article': "Journal Articles",
-		'book_section': "Book Chapters",
-		'conference_item': "Conference Papers/Events",
-	}[x]
-
-def printHeading(heading):
-	print str.format("<h1>{0}</h1>", printableHeading(heading))
+    return {
+        'book': "Books",
+        'article': "Journal Articles",
+        'book_section': "Book Chapters",
+        'conference_item': "Conference Papers/Events",
+    }[x]
 
 
-def printItem(item, eprint_url):
-	# generate the creators list
-	creators = ""
-	
-	for creator in item['creators']:
-		if creators <> "":
-			creators = creators + ", "
+def print_heading(heading):
+    print str.format("<h1>{0}</h1>", printableHeading(heading))
 
-		if creator['id'] is not None:
-			creators = creators + str.format('<a href="{0}view/creators/{3}.html">{1}, {2}</a>', eprint_url, creator['name']['family'].encode('utf8'), creator['name']['given'].encode('utf8'), creator['id'])
-		else:
-			creators = creators + str.format('{0}, {1}', creator['name']['family'].encode('utf8'), creator['name']['given'].encode('utf8'))
 
-	if item['type'] == 'book':
-		print str.format('<li>{0}, <a href="{4}"><i>{1}</i></a> ({2}: {3})</li>', creators, item['title'].encode('utf8'), item['publisher'].encode('utf8'), datetime.strptime(item['date'][0:4], "%Y").year, item['uri'])
+def print_item(item, eprint_url):
+    # generate the creators list
+    creators = ""
 
-	if item['type'] == "article":
-		# build the volume/number format
-		volume = ""
+    for creator in item['creators']:
+        if creators <> "":
+            creators += ", "
 
-		if 'volume' in item:
-			volume = ", "
-			volume = str(item['volume'])
+        if creator['id'] is not None:
+            creators += str.format('<a href="{0}view/creators/{3}.html">{1}, {2}</a>', eprint_url,
+                                   creator['name']['family'].encode('utf8'),
+                                   creator['name']['given'].encode('utf8'), creator['id'])
+        else:
+            creators += str.format('{0}, {1}', creator['name']['family'].encode('utf8'),
+                                   creator['name']['given'].encode('utf8'))
 
-		if 'number' in item:
-			if 'volume' in item:
-				volume = ", "
-			volume = volume + str.format("({0})", str(item['number']))
+    if item['type'] == 'book':
+        print '<li>{0}, <a href="{4}"><i>{1}</i></a> ({2}: {3})</li>'.format(creators,
+                                                                             item['title'].encode('utf8'),
+                                                                             item['publisher'].encode('utf8'),
+                                                                             datetime.strptime(item['date'][0:4],
+                                                                                               "%Y").year,
+                                                                             item['uri'])
 
-		print str.format('<li>{0}, "<a href="{5}">{1}</a>", <i>{2}</i>{3}, {4}</li>', creators, item['title'].encode('utf8'), item['publication'].encode('utf8'), volume, datetime.strptime(item['date'][0:4], "%Y").year, item['uri'])
+    if item['type'] == "article":
+        # build the volume/number format
+        volume = ""
 
-	if item['type'] == "book_section":
-		# generate the editors list
-		editors = ""
+        if 'volume' in item and not 'number' in item:
+            volume = ' ({0})'.format(str(item['volume']))
+        elif 'number' in item and not 'volume' in item:
+            volume = ' {0}'.format(str(item['number']))
+        elif 'number' in item and 'volume' in item:
+            volume = ' {0}({1})'.format(str(item['volume']), str(item['number']))
 
-		if 'editors' in item:
-			for editor in item['editors']:
-				if editors == "":
-					editors = ", ed. by "
-				if editors <> ", ed. by ":
-					editors = editors + ", "
+        print '<li>{0}, "<a href="{5}">{1}</a>", <i>{2}</i>{3}, {4}</li>'.format(creators,
+                                                                                 item['title'].encode('utf8'),
+                                                                                 item['publication'].encode('utf8'),
+                                                                                 volume,
+                                                                                 datetime.strptime(
+                                                                                     item['date'][0:4], "%Y").year,
+                                                                                 item['uri'])
 
-				editors = editors + str.format('{0}, {1}', editor['name']['family'].encode('utf8'), editor['name']['given'].encode('utf8'))
+    if item['type'] == "book_section":
+    # generate the editors list
+        editors = ""
 
-		print str.format('<li>{0}, "<a href="{6}">{1}</a>", in <i>{2}</i>{3} ({4}: {5})</li>', creators, item['title'].encode('utf8'), item['book_title'].encode('utf8'), editors, item['publisher'].encode('utf8'), datetime.strptime(item['date'][0:4], "%Y").year, item['uri'])
+        if 'editors' in item:
+            for editor in item['editors']:
+                if editors == "":
+                    editors = ", ed. by "
+                if editors != ", ed. by ":
+                    editors += editors + ", "
 
-	if item['type'] == "conference_item":
-		print str.format('<li>{0}, "<a href="{4}">{1}</a>", <i>{2}</i> {3}</li>', creators, item['title'].encode('utf8'), item['event_title'].encode('utf8'), datetime.strptime(item['date'][0:4], "%Y").year, item['uri'])
+            editors = editors + '{0}, {1}'.format(editor['name']['family'].encode('utf8'),
+                                                  editor['name']['given'].encode('utf8'))
+
+        print '<li>{0}, "<a href="{6}">{1}</a>", in <i>{2}</i>{3} ({4}: {5})</li>'.format(creators,
+                                                                                          item['title'].encode(
+                                                                                              'utf8'),
+                                                                                          item['book_title'].encode(
+                                                                                              'utf8'),
+                                                                                          editors,
+                                                                                          item['publisher'].encode(
+                                                                                              'utf8'),
+                                                                                          datetime.strptime(
+                                                                                              item['date'][0:4],
+                                                                                              "%Y").year,
+                                                                                          item['uri'])
+
+    if item['type'] == "conference_item":
+        print ('<li>{0}, "<a href="{4}">{1}</a>", <i>{2}</i> {3}</li>'.format(creators,
+                                                                              item['title'].encode('utf8'),
+                                                                              item['event_title'].encode('utf8'),
+                                                                              datetime.strptime(item['date'][0:4],
+                                                                                                "%Y").year,
+                                                                              item['uri']))
+
 
 def main():
-	# read  command line arguments
-	args = docopt(__doc__, version='eprintsCV 0.1')
-	
-	# build the repository path
-	repo = args['<eprints_location>']
+    # read  command line arguments
+    args = docopt(__doc__, version='eprintsCV 0.1')
 
-	if not(repo.startswith("htt")):
-		repo = "http://" + repo
+    # build the repository path
+    repo = args['<eprints_location>']
 
-	if not(repo.endswith("/")):
-		repo = repo + "/"
+    if not (repo.startswith("htt")):
+        repo = "http://" + repo
 
-	url = repo + "cgi/exportview/creators/" + args['<eprints_user>'] + "/JSON/" + args['<eprints_user>'] + ".js"
+    if not (repo.endswith("/")):
+        repo += "/"
 
-	# download the JSON version
-	response = urllib2.urlopen(url)
-	jsonData = response.read()
+    url = repo + "cgi/exportview/creators/" + args['<eprints_user>'] + "/JSON/" + args['<eprints_user>'] + ".js"
 
-	# decode the JSON object into a list of dictionaries
-	# NB that, helpfully, this list is by default reverse sorted (newest first)
-	# should this ever change, it would need a lamda sort:
-	# jsonSorted = sorted(list_to_be_sorted, key=lambda k: k['date']) 
-	jsonList = json.loads(jsonData)
+    # download the JSON version
+    response = urllib2.urlopen(url)
+    json_data = response.read()
 
-	for currentType in args['<list_of_types>'].split(","):
-		# display a heading
-		printHeading(currentType)
-		printStart()
-		for currentItem in jsonList:
-			if currentItem['type'] == currentType:
-				# this is an item of that type
-				printItem(currentItem, repo)
-		printEnd()
+    # decode the JSON object into a list of dictionaries
+    # NB that, helpfully, this list is by default reverse sorted (newest first)
+    # should this ever change, it would need a lamda sort:
+    # jsonSorted = sorted(list_to_be_sorted, key=lambda k: k['date'])
+    json_list = json.loads(json_data)
+
+    for currentType in args['<list_of_types>'].split(","):
+        # display a heading
+        print_heading(currentType)
+        printStart()
+        for currentItem in json_list:
+            if currentItem['type'] == currentType:
+                # this is an item of that type
+                print_item(currentItem, repo)
+        printEnd()
+
 
 if __name__ == '__main__':
     main()
